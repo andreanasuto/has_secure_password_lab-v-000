7702b1c46adb01a85a353c25225edeb2bfcb4f40
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-15 23:45:49 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:name], password: params[:password])
-    binding.pry
     return head(:forbidden) unless @user.authenticate(params[:password])
   end
 end
