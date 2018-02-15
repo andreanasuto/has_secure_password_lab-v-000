3eb504e5d635296ea6f03f84b22b3d0dd18d99cc
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-15 23:52:59 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    sessions[:user_id] = @user.id
+      sessions[:user_id] = @user.id
+    else
+      redirect_to '/users/new'
+    end
   end
 
   private
