998b4d17fa09808ebacd9a92f16bcc35effeee5d
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-15 23:48:59 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
   def create
+    @user = User.new
+
+  end
+
+  private
+
+  def create
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
