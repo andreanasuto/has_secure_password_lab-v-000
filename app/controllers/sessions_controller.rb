8397aed849c541a08db37656f898a8ab6b52a317
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-16 00:17:46 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.last
+    @user = User.find_by(name: params[:name])
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
     else
